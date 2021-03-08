d7e13635a6ae5fed9ce0cef11daa2faf137aed7d
jairosantamaria <jairosantamaria@amelearning.net>
2021-03-08 23:42:20 +0000

    rev02

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Todo } from "./component/home.js";
+import { Todo } from "./component/Todo.js";
 
 //render your react application
 ReactDOM.render(<Todo />, document.querySelector("#app"));
